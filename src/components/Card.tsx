fdfbe2cca6776a17dc80f1286465d4e8a841ffa8
Iqra <iqraanzish48@gmail.com>
2024-11-10 10:21:30 -0800

    first commit

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,55 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import '../app/styles/card.css';
-import { useEffect, useState } from 'react';
 
-
-// components/Card.tsx
-
-
-
-  const [windowLocation, setWindowLocation] = useState('');
-
-
-
-
-
-
-
-
-
-
-
-
-interface propsType{
-    title:string;
-    desc: string;
-    img: string;
-    tags: string[];
+interface propsType {
+  title: string;
+  desc: string;
+  img: string;
+  tags: string[];
 }
 
 const Card: React.FC<propsType> = ({ title, desc, img, tags }) => {
+  // State to store window width
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  // useEffect to update windowWidth only on client side
+  useEffect(() => {
+    // This will only run on the client-side
+    setWindowWidth(window.innerWidth);
+
+    // Optional: Add a resize event listener to update windowWidth on window resize
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // Add event listener for resize
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup function to remove the event listener
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []); // Empty array ensures this runs only once after initial render
+
   return (
-    <div className={`card ${window.innerWidth >= 640 ? 'card-sm': ''}`} data-aos="fade-down">
+    <div className={`card ${windowWidth >= 640 ? 'card-sm' : ''}`} data-aos="fade-down">
+      <div>
+        <Image
+          className={`card-image ${windowWidth >= 640 ? 'card-image-sm' : ''}`}
+          src={img}
+          width={350}
+          height={350}
+          alt={title}
+        />
+      </div>
 
-<div>
-    <Image  className={`card-image ${window.innerWidth >= 640 ? 'card-image-sm' : ''}`}
-    src={img}
-    width={350}
-    height={350}
-    alt={title}
-    />
-</div>
-
-<div className='card-container'>
-    <div className='card-title'>{title}</div>
-    <div>{desc}</div>
-    <div>{tags.map((el) => (<div className='card-tags' key={el}>
-{el}
-    </div>))}
-    </div>
-</div>
-      
+      <div className="card-container">
+        <div className="card-title">{title}</div>
+        <div>{desc}</div>
+        <div>
+          {tags.map((el) => (
+            <div className="card-tags" key={el}>
+              {el}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
